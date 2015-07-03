31f486e69cb0c7657b50ec2e0cc929f098584b6a
bastianlang@t-online.de <bastianlang@t-online.de>
2015-07-03 21:03:17 +0200

    finished cheat sheet 4

--- exercises/src/org/brsu/exercise2/application/ColorTester.java
+++ exercises/src/org/brsu/exercise2/application/ColorTester.java
@@ -1,10 +1,23 @@
 package org.brsu.exercise2.application;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.brsu.exercise2.gui.MainFrame;
+import org.brsu.exercise2.gui.MainPanel;
 
 public class ColorTester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
+	    InvocationTargetException {
 		new MainFrame();
+
+		MainPanel mainPanel = new MainPanel();
+		Method[] methods = mainPanel.getClass().getMethods();
+		for (Method method : methods) {
+			if (method.getName().equalsIgnoreCase("hasFocus")) {
+				System.out.println(method.invoke(mainPanel, null));
+			}
+		}
 	}
 }
