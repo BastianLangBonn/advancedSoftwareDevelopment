a6b30ab4a9ef56f16d4676eb797b3e961138f75b
bastianlang@t-online.de <bastianlang@t-online.de>
2015-07-06 13:50:32 +0200

    refactored board

--- exercises/src/org/brsu/exercise5/model/Board.java
+++ exercises/src/org/brsu/exercise5/model/Board.java
@@ -8,155 +8,160 @@ package org.brsu.exercise5.model;
  */
 public class Board {
 
-  private boolean[][] board;
-  private int width;
-  private int height;
+	private static final int MAX_DIMENSION = 1000000;
+	private boolean[][] board;
+	private int width;
+	private int height;
 
-  public Board() {
-    board = new boolean[10][10];
-    width = 10;
-    height = 10;
-    initializeTiles();
-  }
+	public Board() {
+		board = new boolean[10][10];
+		width = 10;
+		height = 10;
+		initializeTiles();
+	}
 
-  public Board(int width, int height) {
-    board = new boolean[width][height];
-    this.width = width;
-    this.height = height;
-    initializeTiles();
-  }
+	public Board(int width, int height) {
+		if (width > MAX_DIMENSION) {
+			width = MAX_DIMENSION;
+		}
+		if (height > MAX_DIMENSION) {
+			height = MAX_DIMENSION;
+		}
+		board = new boolean[width][height];
+		this.width = width;
+		this.height = height;
+		initializeTiles();
+	}
 
-  public Board(boolean[][] boardConfiguration) {
-    this.width = boardConfiguration.length;
-    this.height = boardConfiguration[0].length;
-    this.board = new boolean[width][height];
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
-        this.board[i][j] = boardConfiguration[i][j];
-      }
-    }
-  }
+	private void initializeTiles() {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				board[i][j] = false;
+			}
+		}
+	}
 
-  public Board(Board board) {
-    width = board.getWidth();
-    height = board.getHeight();
-    this.board = new boolean[width][height];
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
-        this.board[i][j] = board.getTile(i, j);
-      }
-    }
+	public Board(boolean[][] boardConfiguration) {
+		this.width = boardConfiguration.length;
+		this.height = boardConfiguration[0].length;
+		this.board = new boolean[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				this.board[i][j] = boardConfiguration[i][j];
+			}
+		}
+	}
 
-  }
+	public Board(Board board) {
+		width = board.getWidth();
+		height = board.getHeight();
+		this.board = new boolean[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				this.board[i][j] = board.getTile(i, j);
+			}
+		}
+	}
 
-  private void initializeTiles() {
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
-        board[i][j] = false;
-      }
-    }
-  }
+	/**
+	 * Sets a single tile of the board to the given value.
+	 * 
+	 * @param row
+	 * @param column
+	 * @param black
+	 */
+	public void setTile(int width, int height, boolean black) {
+		board[width][height] = black;
+	}
 
-  /**
-   * Sets a single tile of the board to the given value.
-   * 
-   * @param row
-   * @param column
-   * @param black
-   */
-  public void setTile(int width, int height, boolean black) {
-    board[width][height] = black;
-  }
+	public void setBlockOfTiles(int xLeft, int yTop, int xRight, int yBottom) {
+		for (int x = xLeft; x <= xRight; x++) {
+			for (int y = yTop; y <= yBottom; y++) {
+				board[x][y] = true;
+			}
+		}
+	}
 
-  public void setBlockOfTiles(int xLeft, int yTop, int xRight, int yBottom) {
-    for (int x = xLeft; x <= xRight; x++) {
-      for (int y = yTop; y <= yBottom; y++) {
-        board[x][y] = true;
-      }
-    }
-  }
+	/**
+	 * Returns the number of black tiles in the board.
+	 * 
+	 * @return
+	 */
+	public int getNumberOfBlackTilesInBoard() {
+		int result = 0;
+		for (int i = 0; i < width; i++) {
+			result += getNumberOfBlackTilesInColumn(i);
+		}
+		return result;
+	}
 
-  @Override
-  public String toString() {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("[");
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
-        stringBuilder.append(board[i][j]);
-        stringBuilder.append("; ");
-      }
-      stringBuilder.append("\n");
-    }
-    stringBuilder.append("]");
-    return stringBuilder.toString();
-  }
+	/**
+	 * Returns the number of black tiles in the given row.
+	 * 
+	 * @param row
+	 * @return
+	 */
+	public int getNumberOfBlackTilesInRow(int row) {
+		if (row >= height) {
+			return 0;
+		}
+		int result = 0;
+		for (int i = 0; i < width; i++) {
+			if (getTile(i, row)) {
+				result++;
+			}
+		}
+		return result;
+	}
 
-  public static void main(String[] args) {
-    Board solution = new Board(100, 5);
-    System.out.println(solution.toString());
-  }
+	/**
+	 * Returns the number of black tiles in the given column.
+	 * 
+	 * @param column
+	 * @return
+	 */
+	public int getNumberOfBlackTilesInColumn(int column) {
+		if (column >= width) {
+			return 0;
+		}
+		int result = 0;
+		for (int i = 0; i < height; i++) {
+			if (getTile(column, i)) {
+				result++;
+			}
+		}
+		return result;
+	}
 
-  public int getWidth() {
-    return width;
-  }
+	public int getWidth() {
+		return width;
+	}
 
-  public int getHeight() {
-    return height;
-  }
+	public int getHeight() {
+		return height;
+	}
 
-  public boolean getTile(int x, int y) {
-    return board[x][y];
-  }
+	public boolean getTile(int x, int y) {
+		return board[x][y];
+	}
 
-  /**
-   * Returns the number of black tiles in the board.
-   * 
-   * @return
-   */
-  public int getNumberOfBlackTilesInBoard() {
-    int result = 0;
-    for (int i = 0; i < width; i++) {
-      result += getNumberOfBlackTilesInColumn(i);
-    }
-    return result;
-  }
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("[");
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				stringBuilder.append(board[i][j]);
+				stringBuilder.append("; ");
+			}
+			stringBuilder.append("\n");
+		}
+		stringBuilder.append("]");
+		return stringBuilder.toString();
+	}
 
-  /**
-   * Returns the number of black tiles in the given row.
-   * 
-   * @param row
-   * @return
-   */
-  public int getNumberOfBlackTilesInRow(int row) {
-    if (row >= height) {
-      return 0;
-    }
-    int result = 0;
-    for (int i = 0; i < width; i++) {
-      if (getTile(i, row)) {
-        result++;
-      }
-    }
-    return result;
-  }
-
-  /**
-   * Returns the number of black tiles in the given column.
-   * 
-   * @param column
-   * @return
-   */
-  public int getNumberOfBlackTilesInColumn(int column) {
-    if (column >= width) {
-      return 0;
-    }
-    int result = 0;
-    for (int i = 0; i < height; i++) {
-      if (getTile(column, i)) {
-        result++;
-      }
-    }
-    return result;
-  }
-
+	public static void main(String[] args) {
+		Board solution = new Board(100, 5);
+		System.out.println(solution.toString());
+	}
 }
